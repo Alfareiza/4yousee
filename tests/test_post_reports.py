c4225df02bdcc1296d2cfa91cae7f11076b924d3
alfonso <alfareiza@gmail.com>
2022-06-24 18:58:14 -0300

    tests for post reports

---
+++ tests/test_post_reports.py
@@ -0,0 +1,21 @@
+from tests import client
+
+
+def test_post_reports():
+    """Test endpoint post and passing the required fields"""
+
+    filter = {
+        "startDate": "2020-07-26",
+        "startTime": "00:00:00",
+        "endDate": "2020-08-24",
+        "endTime": "23:59:59",
+        "mediaId": [1, 2, 3],
+        "playerId": [2],
+        "sort": -1
+    }
+    response = client.post_reports(filter=filter)
+    assert response.get('filter').get('startDate') == filter['startDate']
+    assert response.get('filter').get('startTime') == filter['startTime']
+    assert response.get('filter').get('endDate') == filter['endDate']
+    assert response.get('filter').get('endDate') == filter['endDate']
+    assert response.get('filter').get('playerId') == filter['playerId']
